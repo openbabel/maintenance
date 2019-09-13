4d9ca478033bde157b3ab434fc1465373558f3ea
Noel O'Boyle <baoilleach@gmail.com>
2019-09-13 10:09:28 +0100

    Add scripts to build on Windows

---
+++ windows/runme.bat
@@ -0,0 +1,16 @@
+echo I recommend deleting the nbu folder if it exists
+pause
+mkdir nbu
+
+git clone https://github.com/openbabel/msvc-dependencies.git nbu\msvc-deps
+rem The following are not needed (and cause problems with) the latest
+rem MSVC2017
+del nbu\msvc-deps\include\stdint.h
+del nbu\msvc-deps\include\inttypes.h
+
+git clone https://github.com/openbabel/openbabel.git nbu\src
+cd nbu\src
+git checkout tags/openbabel-3-0-0a1
+cd ..\..
+call mbuild.bat
+call mbuild_x64.bat
