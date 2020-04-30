b76bde6b68fc361bb2d51db751ef3c6cd714027a
Noel O'Boyle <baoilleach@gmail.com>
2020-04-30 21:03:59 +0100

    Check in updates to build scripts

--- windows/mbuild.bat
+++ windows/mbuild.bat
@@ -1,11 +1,14 @@
-set HERE=C:\Tools\openbabel\maintenance\windows
+set HERE=D:\Noel\openbabel\maintenance\windows
 set DEPS=%HERE%\nbu\msvc-deps
 set BUILDDIR=%HERE%\nbu\build
 set SRCDIR=%HERE%\nbu\src
 
 mkdir %BUILDDIR%
 cd %BUILDDIR%
-set WXWIN=C:\Tools\wxWidgets\wxWidgets-3.0.4
+set WXWIN=D:\Noel\openbabel\wxWidgets\3.0.5
+set SWIGDIR=D:\Noel\openbabel\swig\4.0.1
+set INCHIDIR=D:\Noel\openbebl\inchi\1.0.5
+set JAVADIR=D:\Program Files (x86)\Java\jdk1.8.0_251
 
-"C:\Program Files (x86)\CMake\bin\cmake.exe" -DCMAKE_INSTALL_PREFIX=..\install -G "Visual Studio 15 2017" -DOB_USE_PREBUILT_LIBRARIES=ON -DLIBXML2_LIBRARIES="%DEPS%\libs-common\i386\libxml2.lib" -DMINIMAL_BUILD=OFF -DCAIRO_INCLUDE_DIRS="%DEPS%\include\cairo" -DCAIRO_LIBRARIES="%DEPS%\libs-common\i386\cairo.lib" -DLIBXML2_INCLUDE_DIR=. -DZLIB_LIBRARY="%DEPS%\libs-common\i386\zlib1.lib" -DZLIB_INCLUDE_DIR=. -DINCHI_LIBRARY="%DEPS%\libs-common\i386\libinchi.lib" -DINCHI_INCLUDE_DIR=C:\Tools\InChI\v1.0.5\INCHI-1-SRC\INCHI_API\libinchi\src -DXDR_LIBRARY="%DEPS%\libs-common\i386\xdr.lib" -DXDR_INCLUDE_DIR="%DEPS%\include" -DRUN_SWIG=ON -DJAVA_BINDINGS=ON -DJAVA_INCLUDE_PATH="C:\Program Files (x86)\Java\jdk1.7.0_80\include" -DJAVA_JVM_LIBRARY="C:\Program Files (x86)\Java\jdk1.7.0_80\lib\jvm.lib" -DJAVA_AWT_LIBRARY="C:\Program Files (x86)\Java\jdk1.7.0_80\lib\jawt.lib" -DCSHARP_BINDINGS=ON -DCSHARP_EXECUTABLE=C:\Windows\Microsoft.NET\Framework\v3.5\csc.exe -DENABLE_TESTS=OFF -DBUILD_GUI=ON -DOPENBABEL_USE_SYSTEM_INCHI=TRUE -DwxWidgets_CONFIGURATION=mswu -DEIGEN3_INCLUDE_DIR="%DEPS%\include" -DPYTHON_BINDINGS=ON -DSWIG_EXECUTABLE=C:\Tools\swig\swigwin-3.0.12\swig.exe -DPYTHON_EXECUTABLE=C:\Users\Noel\AppData\Local\Programs\Python\Python37-32\python.exe -DPYTHON_LIBRARY=C:\Users\Noel\AppData\Local\Programs\Python\Python37-32\libs\python37.lib  -DJSON_LIBRARY="%DEPS%\libs-vs12\i386\jsoncpp.lib" %1 %2 %3 %4 %5 %6 %7 %8 %SRCDIR%
+cmake.exe -DCMAKE_INSTALL_PREFIX=..\install -G "Visual Studio 16 2019" -A "Win32" -DOB_USE_PREBUILT_LIBRARIES=ON -DLIBXML2_LIBRARIES="%DEPS%\libs-common\i386\libxml2.lib" -DMINIMAL_BUILD=OFF -DCAIRO_INCLUDE_DIRS="%DEPS%\include\cairo" -DCAIRO_LIBRARIES="%DEPS%\libs-common\i386\cairo.lib" -DLIBXML2_INCLUDE_DIR=. -DZLIB_LIBRARY="%DEPS%\libs-common\i386\zlib1.lib" -DZLIB_INCLUDE_DIR=. -DINCHI_LIBRARY="%DEPS%\libs-common\i386\libinchi.lib" -DINCHI_INCLUDE_DIR=%INCHIDIR%\INCHI-1-SRC\INCHI_API\libinchi\src -DXDR_LIBRARY="%DEPS%\libs-common\i386\xdr.lib" -DXDR_INCLUDE_DIR="%DEPS%\include" -DRUN_SWIG=ON -DJAVA_BINDINGS=ON -DJAVA_INCLUDE_PATH="%JAVADIR%\include" -DJAVA_JVM_LIBRARY="%JAVADIR%\lib\jvm.lib" -DJAVA_AWT_LIBRARY="%JAVADIR%\lib\jawt.lib" -DCSHARP_BINDINGS=ON -DCSHARP_EXECUTABLE=C:\Windows\Microsoft.NET\Framework\v3.5\csc.exe -DENABLE_TESTS=OFF -DBUILD_GUI=ON -DOPENBABEL_USE_SYSTEM_INCHI=TRUE -DwxWidgets_CONFIGURATION=mswu -DEIGEN3_INCLUDE_DIR="%DEPS%\include" -DPYTHON_BINDINGS=ON -DSWIG_EXECUTABLE=%SWIGDIR%\swig.exe -DPYTHON_EXECUTABLE=%USERPROFILE%\AppData\Local\Programs\Python\Python37-32\python.exe -DPYTHON_LIBRARY=%USERPROFILE%\AppData\Local\Programs\Python\Python37-32\libs\python37.lib -DJSON_LIBRARY="%DEPS%\libs-vs12\i386\jsoncpp.lib" %1 %2 %3 %4 %5 %6 %7 %8 %SRCDIR%
 cmake --build . --config Release
--- windows/mbuild_x64.bat
+++ windows/mbuild_x64.bat
@@ -1,12 +1,15 @@
-set HERE=C:\Tools\openbabel\maintenance\windows
+set HERE=D:\noel\openbabel\maintenance\windows
 set DEPS=%HERE%\nbu\msvc-deps
 set BUILDDIR=%HERE%\nbu\build_x64
 set SRCDIR=%HERE%\nbu\src
 
 mkdir %BUILDDIR%
 cd %BUILDDIR%
-set WXWIN=C:\Tools\wxWidgets\wxWidgets-3.0.4
+set WXWIN=D:\Noel\openbabel\wxWidgets\3.0.5
+set SWIGDIR=D:\Noel\openbabel\swig\4.0.1
+set INCHIDIR=D:\Noel\openbebl\inchi\1.0.5
+set JAVADIR="D:\Program Files\Java\jdk1.8.0_251"
 
-"C:\Program Files (x86)\CMake\bin\cmake.exe" -G "Visual Studio 15 2017 Win64" -DOB_USE_PREBUILT_LIBRARIES=ON -DCMAKE_INSTALL_PREFIX=..\install  -DLIBXML2_LIBRARIES="%DEPS%\libs-common\x64\libxml2.lib" -DMINIMAL_BUILD=OFF -DCAIRO_INCLUDE_DIRS="%DEPS%\include\cairo" -DCAIRO_LIBRARIES="%DEPS%\libs-common\x64\cairo.lib" -DLIBXML2_INCLUDE_DIR=. -DZLIB_LIBRARY="%DEPS%\libs-common\x64\zlib1.lib" -DZLIB_INCLUDE_DIR=. -DINCHI_LIBRARY="%DEPS%\libs-common\x64\libinchi.lib" -DINCHI_INCLUDE_DIR=. -DXDR_LIBRARY="%DEPS%\libs-common\x64\xdr.lib" -DXDR_INCLUDE_DIR="%DEPS%\include" -DRUN_SWIG=ON -DJAVA_BINDINGS=ON -DJAVA_INCLUDE_PATH="C:\Program Files\Java\jdk1.7.0_80\include" -DJAVA_JVM_LIBRARY="C:\Program Files\Java\jdk1.7.0_80\lib\jvm.lib" -DJAVA_AWT_LIBRARY="C:\Program Files\Java\jdk1.7.0_80\lib\jawt.lib" -DCSHARP_BINDINGS=ON -DCSHARP_EXECUTABLE=C:\Windows\Microsoft.NET\Framework64\v3.5\csc.exe -DENABLE_TESTS=OFF -DBUILD_GUI=ON -DOPENBABEL_USE_SYSTEM_INCHI=TRUE -DwxWidgets_CONFIGURATION=mswu -DEIGEN3_INCLUDE_DIR="%DEPS%\include" -DPYTHON_BINDINGS=ON -DSWIG_EXECUTABLE=C:\Tools\swig\swigwin-3.0.12\swig.exe -DPYTHON_EXECUTABLE=C:\Users\Noel\AppData\Local\Programs\Python\Python37\python.exe -DPYTHON_LIBRARY=C:\Users\Noel\AppData\Local\Programs\Python\Python37\libs\python37.lib -DJSON_LIBRARY="%DEPS%\libs-vs12\x64\jsoncpp.lib" %1 %2 %3 %4 %5 %6 %7 %8 %SRCDIR%
+cmake.exe -G "Visual Studio 16 2019" -A "x64" -DOB_USE_PREBUILT_LIBRARIES=ON -DCMAKE_INSTALL_PREFIX=..\install -DLIBXML2_LIBRARIES="%DEPS%\libs-common\x64\libxml2.lib" -DMINIMAL_BUILD=OFF -DCAIRO_INCLUDE_DIRS="%DEPS%\include\cairo" -DCAIRO_LIBRARIES="%DEPS%\libs-common\x64\cairo.lib" -DLIBXML2_INCLUDE_DIR=. -DZLIB_LIBRARY="%DEPS%\libs-common\x64\zlib1.lib" -DZLIB_INCLUDE_DIR=. -DINCHI_LIBRARY="%DEPS%\libs-common\x64\libinchi.lib" -DINCHI_INCLUDE_DIR=. -DXDR_LIBRARY="%DEPS%\libs-common\x64\xdr.lib" -DXDR_INCLUDE_DIR="%DEPS%\include" -DRUN_SWIG=ON -DJAVA_BINDINGS=ON -DJAVA_INCLUDE_PATH="%JAVADIR%\include" -DJAVA_JVM_LIBRARY="%JAVADIR%\lib\jvm.lib" -DJAVA_AWT_LIBRARY="%JAVADIR%\lib\jawt.lib" -DCSHARP_BINDINGS=ON -DCSHARP_EXECUTABLE=C:\Windows\Microsoft.NET\Framework64\v3.5\csc.exe -DENABLE_TESTS=OFF -DBUILD_GUI=OFF -DOPENBABEL_USE_SYSTEM_INCHI=TRUE -DwxWidgets_CONFIGURATION=mswu -DEIGEN3_INCLUDE_DIR="%DEPS%\include" -DPYTHON_BINDINGS=ON -DSWIG_EXECUTABLE=%SWIGDIR%\swig.exe -DPYTHON_EXECUTABLE=%USERPROFILE%\AppData\Local\Programs\Python\Python37\python.exe -DPYTHON_LIBRARY=%USERPROFILE%\AppData\Local\Programs\Python\Python37\libs\python37.lib -DJSON_LIBRARY="%DEPS%\libs-vs12\x64\jsoncpp.lib" %1 %2 %3 %4 %5 %6 %7 %8 %SRCDIR%
 cmake --build . --config Release
 rem c:/Windows/Microsoft.NET/Framework64/v4.0.30319/MSBuild.exe /m:4 /p:Configuration=Release ALL_BUILD.vcxproj
