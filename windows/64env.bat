55a3b9cf63027f37c2a06efbe85b25634fe62bed
Noel O'Boyle <baoilleach@gmail.com>
2020-05-02 14:05:40 +0100

    Updates

--- windows/64env.bat
+++ windows/64env.bat
@@ -6,9 +6,9 @@ set SRCDIR=%HERE%\nbu\src
 set WXWIN=D:\Noel\openbabel\wxWidgets\3.0.5
 set SWIGDIR=D:\Noel\openbabel\swig\4.0.1
 set INCHIDIR=D:\Noel\openbebl\inchi\1.0.5
-set JAVADIR=D:\Program Files (x86)\Java\jdk1.8.0_251
+set JAVADIR=D:\Program Files\Java\jdk1.8.0_251
 
-set PATH=%DEPS%\libs-common\i386
-set PATH=%DEPS%\libs-vs12\i386;%PATH%
+set PATH=%DEPS%\libs-common\x64
+set PATH=%DEPS%\libs-vs12\x64;%PATH%
 set PATH=%INCHIDIR%\INCHI-1-SRC\INCHI_API\bin\Windows\Win32\Release;%PATH%
-set PATH=%WXWIN%\lib\vc142_dll;%PATH%
+set PATH=%WXWIN%\lib\vc142_x64_dll;%PATH%
